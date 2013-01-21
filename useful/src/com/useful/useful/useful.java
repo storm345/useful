f5b45ff37904d6fe0da427b221ab6d188091568d
storm345 <eddiebbrown05@gmail.com>
2013-01-21 20:01:08 +0000

    Changed a comment in main class

--- useful/src/com/useful/useful/useful.java
+++ useful/src/com/useful/useful/useful.java
@@ -72,7 +72,7 @@ import com.useful.useful.utils.*;
 import lib.PatPeter.SQLibrary.*;
  
 public class useful extends JavaPlugin {
-	//The useful plugin's code!
+	//The useful plugin's java code!
 	public static String pluginFolder;
     public SQLite sqlite;
 	public ListStore heros;
